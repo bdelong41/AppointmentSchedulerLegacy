301fdd1617273ad6719ee6275bf3fc25ea27fb48
= <bdelong.201@gmail.com>
2023-05-01 23:40:14 -0700

    InitialCommit

---
+++ src/main/java/com/example/main/model/Contact.java
@@ -0,0 +1,54 @@
+package com.example.main.model;
+
+public class Contact {
+    private String contactName;
+    private String email;
+    private final Integer id;
+
+    /**
+     * default constructor of Contact.
+     * @param id - Sets the contact id.
+     */
+    public Contact(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return contactName Returns the contact name.
+     */
+    public String getContactName() {
+        return contactName;
+    }
+    /**
+     * @return id Returns the contact id.
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @return email Returns the contact's email.
+     */
+    public String getEmail() {
+        return email;
+    }
+    /**
+     * @param contactName Sets the contact name.
+     */
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+    /**
+     * @param email Sets the contact name.
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    /**
+     * @return Returns the contact's name.
+     */
+    @Override
+    public String toString(){
+        return contactName;
+    }
+}
